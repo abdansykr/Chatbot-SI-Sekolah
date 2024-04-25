46cedeff59262d65717b255ff1f0676c8df5c71c
Abdans23 <abdan2100018117@webmail.uad.ac.id>
2024-04-25 20:50:40 +0700

    Commit App

---
+++ data_kurikulum.sql
@@ -0,0 +1,45 @@
+CREATE TABLE IF NOT EXISTS kurikulum(
+   no             INTEGER  NOT NULL PRIMARY KEY 
+  ,kelas          INTEGER  NOT NULL
+  ,mata_pelajaran VARCHAR(50) NOT NULL
+  ,jam_mp_minggu  INTEGER  NOT NULL
+);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (1,10,'Matematika',3);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (2,10,'Bahasa Indonesia',2);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (3,10,'Bahasa Inggris',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (4,10,'Pendidikan Pancasila',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (5,10,'Fisika',2);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (6,10,'Kimia',2);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (7,10,'Biologi',2);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (8,10,'IPS',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (9,10,'Seni Budaya',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (10,10,'Informatika',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (11,10,'Agama',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (12,10,'PJOK',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (13,10,'Keterampilan Pilihan',4);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (14,11,'Matematika',3);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (15,11,'Bahasa Indonesia',2);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (16,11,'Bahasa Inggris',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (17,11,'Pendidikan Pancasila',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (18,11,'Fisika',2);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (19,11,'Kimia',2);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (20,11,'Biologi',2);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (21,11,'IPS',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (22,11,'Seni Budaya',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (23,11,'Informatika',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (24,11,'Agama',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (25,11,'PJOK',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (26,11,'Keterampilan Pilihan',4);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (27,12,'Matematika',3);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (28,12,'Bahasa Indonesia',2);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (29,12,'Bahasa Inggris',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (30,12,'Pendidikan Pancasila',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (31,12,'Fisika',2);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (32,12,'Kimia',2);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (33,12,'Biologi',2);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (34,12,'IPS',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (35,12,'Seni Budaya',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (36,12,'Informatika',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (37,12,'Agama',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (38,12,'PJOK',1);
+INSERT INTO kurikulum(no,kelas,mata_pelajaran,jam_mp_minggu) VALUES (39,12,'Keterampilan Pilihan',4);
